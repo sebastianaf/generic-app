215f6fa5e873e21199f6cb9c9a2a56c8157dc55b
sebastianaf <sebastianafanador95@gmail.com>
2022-07-22 10:19:34 -0500

    v1.4.1
    erasing console.log

--- api-01/src/middlewares/auth.handler.js
+++ api-01/src/middlewares/auth.handler.js
@@ -23,7 +23,6 @@ const authHandler = (req, res, next) => {
       }
     }
   } catch (error) {
-    //console.log(error);
     res.status(403).send(boom.unauthorized());
     next(boom.unauthorized());
   }
